e4401b3e894571df1fdc71770f1d0358c78df8df
BamoJ <94501520+BamoJ@users.noreply.github.com>
2024-03-17 01:47:48 +0700

    ~~~

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,5 +1,5 @@
-import { Content } from '@prismicio/client'
-import { SliceComponentProps } from '@prismicio/react'
+import { Content } from "@prismicio/client"
+import { SliceComponentProps } from "@prismicio/react"
 
 /**
  * Props for `Hero`.
@@ -15,12 +15,16 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <div className='flex w-full flex-col gap-[-0.15rem]'>
-        <h1 className='text-2xl font-bold tracking-tight'>
-          {slice.primary.first_name}
-          {slice.primary.last_name}
+      <div className="grid grid-cols-1 items-center desktop:grid-cols-2">
+        <h1
+          className="text-[clamp(3rem,20vmin,20rem)]"
+          aria-label={slice.primary.first_name + " " + slice.primary.last_name}
+        >
+          {slice.primary.first_name} {slice.primary.last_name}
         </h1>
-        <span>{slice.primary.title}</span>
+        <span className="text-lg uppercase tracking-wider">
+          {slice.primary.title}
+        </span>
       </div>
     </section>
   )
